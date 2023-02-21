240a17cb413445730a33261308740d4580fee000
dlj <1547551591@qq.com>
2023-02-21 11:30:27 +0800

    fix: 修改logo标的字体链接

--- src/components/TopHeader/index.jsx
+++ src/components/TopHeader/index.jsx
@@ -27,7 +27,7 @@ export default function TopHeader(props) {
     return (
         <div className='top-header'>
             <div className='top-header_logo'>
-                <span class="css2f4d7265f221dc9">飞识</span>
+                <span className='css350f6ba41421dc9'>飞识</span>
             </div>
             <TopTabs/>
             <Search content={content}/>
